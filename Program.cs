7f94f65279206d2dd6253494e21e175c89d667bf
yowisz <bezwstepu@gmail.com>
2018-10-14 15:45:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
